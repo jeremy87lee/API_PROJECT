190c27f6eaf975c512bc563f0859d407e34205e1
jeremy87lee <jeremylovell2004@gmail.com>
2026-08-05 14:18:24 -0400

    fixed user routes and created Flight GET and POST routes

--- App/views/API.py
+++ App/views/API.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request, flash, redirect, url_for
 from flask_jwt_extended import jwt_required, current_user as jwt_current_user
-from App.controllers.user import get_all_users_json,create_user
+from App.controllers.user import get_all_users_json,create_user,get_all_flights_json,create_Flight,get_all_users
 
 api_views = Blueprint('api_views',__name__, url_prefix='/api')
 
@@ -19,8 +19,31 @@ def get_users():
 @jwt_required()
 def create_general_user():
     data = request.json
-    user = create_user(data['username'],data['password'],data['is_admin'])
+    if data.get("username") is None or data.get("password") is None or data.get("is_admin") is None:
+        return jsonify({"message":f"Missing credentials!"}),401
+    users = get_all_users()
+    for u in users:
+        if u.username == data.get("username"):
+            return jsonify({"message":f"Username already taken!"}),401
+    user = create_user(data.get("username"),data.get("password"),data.get("is_admin"))
     if user:
         return jsonify({"message":f"User created!"}),201
     return jsonify({"message":f"User could not be created!"}),400
 
+'Flight API endpoints'
+@api_views.route('/flights',methods=['GET'])
+@jwt_required()
+def get_flights():
+    flights = get_all_flights_json()
+    return jsonify(flights),200
+
+@api_views.route('/create_flight',methods=['POST'])
+@jwt_required()
+def create_flight_function():
+    data = request.json
+    if data.get("departure_time") is None or data.get("arrival_time") is None or data.get("plane_id") is None or data.get("pilot_id") is None or data.get("departure_destination") is None or data.get("arrival_destination") is None:
+        return jsonify({"message" : f"Missing flight data! Could not be created!"}),400
+    new_flight = create_Flight(data.get("departure_time"),data.get("arrival_time"),data.get("plane_id"),data.get("pilot_id"),data.get("departure_destination"),data.get("arrival_destination"))
+    if new_flight:
+        return jsonify({"message":f"Flight created!"}),201
+    return jsonify({"message":f"Flight could not be created!"}),400
