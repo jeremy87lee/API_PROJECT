0ad4eda523b1403b404bcdf28883fc71f44e7a46
jeremy87lee <jeremylovell2004@gmail.com>
2026-08-06 20:34:40 -0400

    Get all pilots endpoint

--- App/views/API.py
+++ App/views/API.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify, request, flash, redirect, url_for
 from flask_jwt_extended import jwt_required, current_user as jwt_current_user, set_access_cookies
 from App.controllers.user import get_all_users_json,create_user,get_all_flights_json,create_Flight,get_all_users,update_flight,delete_flight
 from App.controllers.user import get_all_planes_json, create_Plane, update_plane, delete_plane
+from App.controllers.user import get_all_pilots_json
 from App.controllers import login,initialize
 from App.models.Flights import Flight,Plane
 
@@ -145,3 +146,9 @@ def delete_plane_function(plane_id):
     if not deletion:
         return jsonify({"message":f"Plane number {plane_id} not deleted!"}),400
     return jsonify({"message":f"Plane {plane_id} deleted!"}),200
+
+@api_views.route('/pilots',methods=['GET'])
+@jwt_required()
+def display_pilots():
+    pilots = get_all_pilots_json()
+    return jsonify(pilots)
