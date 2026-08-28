1a93f1b2d2bdf373d75d91f85753ba1187d33dbd
jeremy87lee <jeremylovell2004@gmail.com>
2026-08-28 09:58:26 -0400

    UI pagination and sorting for gates

--- flight-frontend/src/Gates.jsx
+++ flight-frontend/src/Gates.jsx
@@ -7,12 +7,21 @@ import NavBar from "./navBar";
 function GatesPage(){
     const navigate = useNavigate()
     const [gates,setGates] = useState([])
+    const [page,setPage] = useState(1)
+    const [perPage,setPerPage] = useState(3)
+    const [totalPages,setTotalPages] = useState(1)
+    const [sort,setSort] = useState("")
 
     const fetch = async() => {
-        const response = await apiFetch("/gates")
+        const params = new URLSearchParams()
+        params.set("page",page)
+        params.set("per_page",perPage)
+        params.set("Sort",sort) && sort
+        const response = await apiFetch(`/gates?${params.toString()}`)
         if(response.ok){
             const data = await response.json()
             setGates(data.data)
+            setTotalPages(data.total_pages)
             console.log("good fetch")
         }
         if(response.status == 401){
@@ -22,18 +31,30 @@ function GatesPage(){
 
     useEffect(() => {
         fetch()
-    },[]);
+    },[page,sort]);
 
 
 
     return(
         <div>
             <NavBar />
+            <div>
+                <select value={sort} onChange={(e) => {setPage(1);setSort(e.target.value)}}>
+                    <option value="">No Sort</option>
+                    <option value="terminal">Terminal (ascending)</option>
+                    <option value="-terminal">Terminal (descending)</option>
+                </select>
+            </div>
             <ul>
                 {gates.map((gate) => (
                     <li>Gate number {gate.id} - Gate terminal {gate.terminal} - Gate flight {gate.flight} </li>
                 ))}
             </ul>
+            <div>
+                <button disabled={page <= 1} onClick={() => {setPage(page-1)}}>Previous</button>
+                <spam>page {page} of {totalPages}</spam>
+                <button disabled={page >= totalPages} onClick={() => {setPage(page+1)}}>Next</button>
+            </div>
         </div>
     );
 }
