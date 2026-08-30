e1299cb22c49c117828e7e2b7e83c03b529f7164
jeremy87lee <jeremylovell2004@gmail.com>
2026-08-30 12:53:10 -0400

    Revert "implemented seperated creation for admins and general users"
    This reverts commit 52e080a3c6e1f6217a5b8eb3ca82ac7b8861c502.

--- wsgi.py
+++ wsgi.py
@@ -76,20 +76,11 @@ app.cli.add_command(cli_client)
 @cli_client.command("create user",help="Command to create a new user")
 @click.argument("username",default="jeremy")
 @click.argument("password",default="jeremypass")
-def create_user_command(username,password):
+@click.argument("is_admin",default=False)
+def create_user_command(username,password,is_admin):
     response = requests.post(
         "https://api-project-chbu.onrender.com/api/create_user",
-        json={"username": username,"password":password}
-    )
-    print(response.json().get("message"))
-
-@cli_client.command("create admin",help="Command to create a new admin user")
-@click.argument("username",default="admin")
-@click.argument("password",default="adminpass")
-def create_admin_command(username,password):
-    response = requests.post(
-        "https://api-project-chbu.onrender.com/api/create_admin",
-        json={"username": username,"password":password}
+        json={"username": username,"password":password,"is_admin":is_admin}
     )
     print(response.json().get("message"))
 
