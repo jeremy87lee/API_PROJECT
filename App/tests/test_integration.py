b6c6b3d5b5ae44b3174f72c4b0ef4eba4c64550c
jeremy87lee <jeremylovell2004@gmail.com>
2026-08-30 13:09:32 -0400

    implemented creation seperation

--- App/tests/test_integration.py
+++ App/tests/test_integration.py
@@ -26,16 +26,14 @@ def test_get_users(empty_db):
 def test_create_user(empty_db):
         response = empty_db.post('/api/create_user',json={
             "username": "greg",
-            "password": "gregpass",
-            "is_admin": False
+            "password": "gregpass"
         })
         assert response.status_code == 201
         assert "User created!" == response.get_json().get("message")
 
 def test__user_creation_missing_credentials(empty_db):
     response = empty_db.post('/api/create_user',json={
-                "password": "gregpass",
-                "is_admin": False
+                "password": "gregpass"
             })
     assert response.status_code == 400
     assert "Missing credentials!" == response.get_json().get("message")
@@ -43,14 +41,12 @@ def test__user_creation_missing_credentials(empty_db):
 def test_user_creation_username_taken(empty_db):
     empty_db.post("/api/create_user",json={
         "username": "bob",
-        "password": "bobpass",
-        "is_admin": False
+        "password": "bobpass"
     })
     
     response = empty_db.post("/api/create_user",json={
             "username": "bob",
-            "password": "bobpass",
-            "is_admin": False
+            "password": "bobpass"
         })
     
     assert response.status_code == 409
@@ -72,13 +68,12 @@ def test_user_login_bad_credentials(empty_db):
     assert response.get_json().get("message") == "bad username or password given"
 
 def test_create_admin(empty_db):
-    response = empty_db.post("/api/create_user",json={
+    response = empty_db.post("/api/create_admin",json={
                 "username": "admin",
-                "password": "adminpass",
-                "is_admin": True
+                "password": "adminpass"
             })
     assert response.status_code == 201
-    assert "User created!" == response.get_json().get("message")
+    assert "Admin user created!" == response.get_json().get("message")
 
 def test_admin_login(empty_db):
     response = empty_db.post("/api/login",json={
